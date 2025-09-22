08fd0dbd77b792bd191b6000dde60ff17e0f7f10
marco76tv <marco.sottana@gmail.com>
2025-09-22 21:02:40 +0200

    .

--- app/Filament/Resources/HasMediaResource/RelationManagers/MediaRelationManager.php
+++ app/Filament/Resources/HasMediaResource/RelationManagers/MediaRelationManager.php
@@ -7,7 +7,11 @@ namespace Modules\Media\Filament\Resources\HasMediaResource\RelationManagers;
 use Filament\Actions\Action;
 use Filament\Actions\ActionGroup;
 use Override;
+<<<<<<< HEAD
 use Filament\Schemas\Schema;
+=======
+use Filament\Forms\Form;
+>>>>>>> 6144976 (.)
 use Illuminate\Database\Eloquent\Model;
 use Modules\Media\Filament\Resources\HasMediaResource\Actions\AddAttachmentAction;
 use Modules\Media\Filament\Resources\MediaResource;
