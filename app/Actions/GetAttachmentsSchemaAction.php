6584809fd05c95f257ccc12427cad94457e7667f
marco76tv <marco.sottana@gmail.com>
2025-09-23 16:35:52 +0200

    .

--- app/Actions/GetAttachmentsSchemaAction.php
+++ app/Actions/GetAttachmentsSchemaAction.php
@@ -10,15 +10,7 @@ use Filament\Forms;
 use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
 use Filament\Forms\Components\TextInput;
-<<<<<<< HEAD
-<<<<<<< HEAD
-use Filament\Forms\Form;
-=======
 use Filament\Schemas\Schema;
->>>>>>> e0ce06e (.)
-=======
-use Filament\Schemas\Schema;
->>>>>>> 365a876 (.)
 use Filament\Pages\SubNavigationPosition;
 use Filament\Resources\Resource as FilamentResource;
 use Illuminate\Contracts\Support\Htmlable;
@@ -59,7 +51,7 @@ class GetAttachmentsSchemaAction
                 ->required()
                 ->previewable(false)
                 //->saveUploadedFiles()
-                ->afterStateUpdated(function ($state, \Filament\Schemas\Components\Utilities\Set $set) use ($attachment, $sessionDir, $disk) {
+                ->afterStateUpdated(function ($state, Set $set) use ($attachment, $sessionDir, $disk) {
                     if (!$state)
                         return;
                     $state = Arr::wrap($state);
