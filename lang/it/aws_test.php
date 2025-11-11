5e49875ce0b2ff6c93d273d23a798fbcd9b12d3f
marco76tv <marco.sottana@gmail.com>
2025-11-11 14:05:15 +0100

    .

--- lang/it/aws_test.php
+++ lang/it/aws_test.php
@@ -3,19 +3,7 @@
 declare(strict_types=1);
 
 return [
-<<<<<<< HEAD
-<<<<<<< HEAD
     'navigation' => [
         'group' => 'Media',
     ],
-=======
-  'navigation' => [
-    'group' => 'Media',
-  ],
->>>>>>> 0a466ed (.)
-=======
-    'navigation' => [
-        'group' => 'Media',
-    ],
->>>>>>> 37a2da6 (.)
 ];
