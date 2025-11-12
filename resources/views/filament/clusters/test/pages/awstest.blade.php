ffc5bbee140fde25ddf3fc0eb9d9f094d74dcfd9
marco76tv <marco.sottana@gmail.com>
2025-11-12 11:28:13 +0100

    rebase 300

--- resources/views/filament/clusters/test/pages/awstest.blade.php
+++ resources/views/filament/clusters/test/pages/awstest.blade.php
@@ -167,6 +167,7 @@ declare(strict_types=1);
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> 47a54fe (.)
 =======
@@ -307,7 +308,12 @@ declare(strict_types=1);
 >>>>>>> 232bbba (.)
 =======
 >>>>>>> a70c4fa (.)
+=======
+>>>>>>> b2850c2 (.)
 <x-filament::page>
+=======
+<x-fi<x-filament::page>
+>>>>>>> e0ce06e (.)
     @php
         // Ensure $results is defined with a default value
         $results = $results ?? null;
