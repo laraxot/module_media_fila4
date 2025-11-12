4ecafe254307387aaf77499f13f67e64b7b8c5d5
marco76tv <marco.sottana@gmail.com>
2025-11-12 11:27:52 +0100

    rebase 300

--- resources/views/filament/clusters/test/pages/s3test.blade.php
+++ resources/views/filament/clusters/test/pages/s3test.blade.php
@@ -171,7 +171,11 @@ declare(strict_types=1);
     ✅ Accessibile: Supporto screen reader
 --}}
 
+<<<<<<< HEAD
 <x-filament::page>
+=======
+<x-fi<x-filament::page>
+>>>>>>> e0ce06e (.)
     <x-filament::section>
         <x-slot name="heading">
             {{ __('ui::pages.s3test.heading') }}
