e5e027ee51ecfbfebcfe3f28a618a53a4bae9fbb
marco76tv <marco.sottana@gmail.com>
2025-11-12 11:28:22 +0100

    rebase 300

--- tests/Pest.php
+++ tests/Pest.php
@@ -675,23 +675,42 @@ expect()->extend('toBeMediaCollection', function () {
 =======
 >>>>>>> 53e059b (.)
 
+/**
+ * @param array<string, mixed> $attributes
+ */
 function createMedia(array $attributes = []): Media
 {
-    return Media::factory()->create($attributes);
+    $Media = Media::factory()->create($attributes);
+    assert($Media instanceof Media);
+    return $Media;
 }
 
+/**
+ * @param array<string, mixed> $attributes
+ */
 function makeMedia(array $attributes = []): Media
 {
-    return Media::factory()->make($attributes);
+    $Media = Media::factory()->make($attributes);
+    assert($Media instanceof Media);
+    return $Media;
 }
 
+/**
+ * @param array<string, mixed> $attributes
+ */
 function createMediaCollection(array $attributes = []): MediaCollection
 {
-    return MediaCollection::factory()->create($attributes);
+    $MediaCollection = MediaCollection::factory()->create($attributes);
+    assert($MediaCollection instanceof MediaCollection);
+    return $MediaCollection;
 }
 
+/**
+ * @param array<string, mixed> $attributes
+ */
 function makeMediaCollection(array $attributes = []): MediaCollection
 {
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -846,4 +865,9 @@ function makeMediaCollection(array $attributes = []): \Modules\Media\Models\Medi
 >>>>>>> da2a819 (.)
 =======
 >>>>>>> 53e059b (.)
+=======
+    $MediaCollection = MediaCollection::factory()->make($attributes);
+    assert($MediaCollection instanceof MediaCollection);
+    return $MediaCollection;
+>>>>>>> 4b10075 (.)
 }
