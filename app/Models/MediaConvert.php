0ed0524157a5842c1beccab31f8acd5559e14efd
marco76tv <marco.sottana@gmail.com>
2025-11-12 11:27:54 +0100

    rebase 300

--- app/Models/MediaConvert.php
+++ app/Models/MediaConvert.php
@@ -217,13 +217,19 @@ class MediaConvert extends BaseModel
     public function getDiskAttribute(?string $value): ?string
 >>>>>>> f41e45e (.)
     {
-        if ($this->media === null) {
+        $media = $this->media;
+        if ($media === null) {
             return null;
         }
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 
         return $this->media->disk;
+=======
+        $disk = $media->getAttribute('disk');
+        return is_string($disk) ? $disk : null;
+>>>>>>> 8bb13d7 (.)
     }
 
     public function getFileAttribute(?string $value): ?string
@@ -241,13 +247,23 @@ class MediaConvert extends BaseModel
     public function getFileAttribute(?string $value): ?string
 >>>>>>> f41e45e (.)
     {
-        if ($this->media === null) {
+        $media = $this->media;
+        if ($media === null) {
             return null;
         }
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 
         return $this->media->path.'/'.$this->media->file_name;
+=======
+        $path = $media->getAttribute('path');
+        $fileName = $media->getAttribute('file_name');
+        if (! is_string($path) || ! is_string($fileName)) {
+            return null;
+        }
+        return $path.'/'.$fileName;
+>>>>>>> 8bb13d7 (.)
     }
 
     public function getConvertedFileAttribute(?string $value): ?string
@@ -265,15 +281,23 @@ class MediaConvert extends BaseModel
     public function getConvertedFileAttribute(?string $value): ?string
 >>>>>>> f41e45e (.)
     {
-        if ($this->media === null) {
+        $media = $this->media;
+        if ($media === null) {
             return null;
         }
-        $info = pathinfo($this->media->file_name);
+        $fileName = $media->getAttribute('file_name');
+        $path = $media->getAttribute('path');
+        $format = $this->getAttribute('format');
+        if (! is_string($fileName) || ! is_string($path) || ! is_string($format)) {
+            return null;
+        }
+        $info = pathinfo($fileName);
         // "dirname" => "."
         // "basename" => "20600550-uhd_3840_2160_30fps.mp4"
         // "extension" => "mp4"
         // "filename" => "20600550-uhd_3840_2160_30fps"
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
         return $this->media->path.'/conversions/'.$info['filename'].'_'.$this->id.'.'.$this->format;
@@ -283,5 +307,8 @@ class MediaConvert extends BaseModel
 =======
         return $this->media->path.'/conversions/'.$info['filename'].'_'.$this->id.'.'.$this->format;
 >>>>>>> f41e45e (.)
+=======
+        return $path.'/conversions/'.$info['filename'].'_'.$this->id.'.'.$format;
+>>>>>>> 8bb13d7 (.)
     }
 }
