e0257a7892910587cc01dba3a35790fa1b9692ee
marco76tv <marco.sottana@gmail.com>
2025-11-12 11:27:53 +0100

    rebase 300

--- app/Actions/GetAttachmentsSchemaAction.php
+++ app/Actions/GetAttachmentsSchemaAction.php
@@ -41,11 +41,7 @@ class GetAttachmentsSchemaAction
 {
     public function execute(array $attachments, string $disk = 'attachments'): array
     {
-<<<<<<< HEAD
         $form = [];
-=======
-        $schema = [];
->>>>>>> 739e4b7 (.)
         $sessionId = session()->getId();
         $prefix = Config::string('media-library.prefix');
 
@@ -62,9 +58,9 @@ class GetAttachmentsSchemaAction
             $sessionDir = $prefix . '/' . $sessionDir;
         }
         foreach ($attachments as $attachment) {
-<<<<<<< HEAD
             $form[$attachment] = FileUpload::make($attachment)
                 //$form[$attachment]=SpatieMediaLibraryFileUpload::make($attachment)
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -91,6 +87,8 @@ class GetAttachmentsSchemaAction
 >>>>>>> 71435ff (.)
 =======
 >>>>>>> c3b81ef (.)
+=======
+>>>>>>> 9a7a2fa (.)
                 ->directory($sessionDir)
                 ->disk($disk)
                 ->acceptedFileTypes(['application/pdf', 'image/jpeg', 'image/png', 'image/jpg'])
@@ -139,10 +137,6 @@ class GetAttachmentsSchemaAction
                 });
         }
 
-<<<<<<< HEAD
         return $form;
-=======
-        return $schema;
->>>>>>> 739e4b7 (.)
     }
 }
