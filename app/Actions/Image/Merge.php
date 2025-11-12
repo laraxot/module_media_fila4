6850eddd92e1b03c2ba7d0801b78eaab4f73e13d
marco76tv <marco.sottana@gmail.com>
2025-11-12 11:27:41 +0100

    rebase 300

--- app/Actions/Image/Merge.php
+++ app/Actions/Image/Merge.php
@@ -9,8 +9,14 @@ use Intervention\Image\Drivers\Gd\Driver as GdDriver;
 use Intervention\Image\ImageManager as InterventionImageManager;
 =======
 use Intervention\Image\ImageManager as InterventionImageManager;
+<<<<<<< HEAD
 use Intervention\Image\Drivers\Gd\Driver as GdDriver;
+<<<<<<< HEAD
 >>>>>>> 5200b63 (.)
+=======
+=======
+>>>>>>> e361059 (.)
+>>>>>>> 9932c65 (.)
 
 class Merge
 {
@@ -30,6 +36,7 @@ class Merge
      */
     public function handle(string $path1, string $path2, string $outputPath): bool
     {
+<<<<<<< HEAD
         // Intervention Image v3: il costruttore richiede un DriverInterface
 <<<<<<< HEAD
         $manager = new InterventionImageManager(new GdDriver);
@@ -43,6 +50,17 @@ class Merge
 
         // Inserisce image2 sopra image1 (centrato) - v3 usa place()
         $image1->place($image2, 'center');
+=======
+        // Compatibile con Intervention Image v2 (Laravel 10):
+        $manager = new InterventionImageManager(['driver' => 'gd']);
+
+        // Carica le immagini
+        $image1 = $manager->make($path1);
+        $image2 = $manager->make($path2);
+
+        // Inserisce image2 sopra image1 (centrato)
+        $image1->insert($image2, 'center');
+>>>>>>> e361059 (.)
 
         // Salva il risultato
         $image1->save($outputPath);
