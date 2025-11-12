f16ea46dda3bfa4f0e05a564a2aa60f3560fa93e
marco76tv <marco.sottana@gmail.com>
2025-11-12 11:27:59 +0100

    rebase 300

--- tests/Filament/Resources/mediaconvertresourcetest.php
+++ tests/Filament/Resources/mediaconvertresourcetest.php
@@ -15,11 +15,7 @@ test('get form schema returns expected components', function (): void {
     expect($schema)->toBeArray();
     expect($schema)->not->toBeEmpty();
 
-<<<<<<< HEAD
     $componentClasses = array_map(get_class(...), $schema);
-=======
-    $componentClasses = array_map(fn ($c) => get_class($c), $schema);
->>>>>>> 9348522 (.)
 
     expect($componentClasses)->toContain(Radio::class);
     expect($componentClasses)->toContain(TextInput::class);
