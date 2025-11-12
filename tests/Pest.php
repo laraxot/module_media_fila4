8912cff68f107f4321218565214e89e0ba5778a8
marco76tv <marco.sottana@gmail.com>
2025-11-12 11:28:23 +0100

    rebase 300

--- tests/Pest.php
+++ tests/Pest.php
@@ -141,7 +141,6 @@ use Modules\Media\Tests\TestCase;
  * |
  */
 
-/** @phpstan-ignore-next-line method.internalClass */
 pest()->extend(TestCase::class)->in('Feature', 'Unit');
 
 /*
@@ -155,10 +154,9 @@ pest()->extend(TestCase::class)->in('Feature', 'Unit');
  * |
  */
 
-expect()->extend('toBeMedia', function (): mixed {
-    /** @phpstan-ignore-next-line variable.undefined */
-    return $this->toBeInstanceOf(Media::class);
-});
+expect()->extend('toBeMedia', fn () => $this->toBeInstanceOf(Media::class));
+
+expect()->extend('toBeMediaCollection', fn () => $this->toBeInstanceOf(MediaCollection::class));
 
 /*
  * |--------------------------------------------------------------------------
@@ -677,51 +675,24 @@ expect()->extend('toBeMediaCollection', function () {
 =======
 >>>>>>> 53e059b (.)
 
-/**
- * @param array<string, mixed> $attributes
- */
 function createMedia(array $attributes = []): Media
 {
-    $Media = Media::factory()->create($attributes);
-    assert($Media instanceof Media);
-    return $Media;
+    return Media::factory()->create($attributes);
 }
 
-/**
- * @param array<string, mixed> $attributes
- */
 function makeMedia(array $attributes = []): Media
 {
-    $Media = Media::factory()->make($attributes);
-    assert($Media instanceof Media);
-    return $Media;
+    return Media::factory()->make($attributes);
 }
 
-/**
- * @param array<string, mixed> $attributes
- * @return MediaCollection
- * @phpstan-ignore-next-line class.notFound
- */
 function createMediaCollection(array $attributes = []): MediaCollection
 {
-    /** @var \Illuminate\Database\Eloquent\Factories\Factory<MediaCollection> $factory */
-    /** @phpstan-ignore-next-line class.notFound */
-    $factory = MediaCollection::factory();
-
-    /** @var MediaCollection $mediaCollection */
-    /** @phpstan-ignore-next-line class.notFound */
-    $mediaCollection = $factory->create($attributes);
-
-    return $mediaCollection;
+    return MediaCollection::factory()->create($attributes);
 }
 
-/**
- * @param array<string, mixed> $attributes
- * @return MediaCollection
- * @phpstan-ignore-next-line class.notFound
- */
 function makeMediaCollection(array $attributes = []): MediaCollection
 {
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -888,4 +859,7 @@ function makeMediaCollection(array $attributes = []): \Modules\Media\Models\Medi
 >>>>>>> 4b10075 (.)
 =======
 >>>>>>> 8e41d16 (.)
+=======
+    return MediaCollection::factory()->make($attributes);
+>>>>>>> 9803aa5 (.)
 }
