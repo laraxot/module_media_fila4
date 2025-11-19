2d15ec078291639d88e5fa7a0946be788a9b6685
marco76tv <marco.sottana@gmail.com>
2025-11-19 12:12:11 +0100

    .

--- app/Filament/Resources/MediaResource/Widgets/ConvertWidget.php
+++ app/Filament/Resources/MediaResource/Widgets/ConvertWidget.php
@@ -261,17 +261,17 @@ class ConvertWidget extends Widget
         /** @phpstan-ignore-next-line - FFMpeg fluent API */
         $toDiskMedia = $exportedMedia->toDisk($disk_mp4);
         if ($toDiskMedia === null) {
-            throw new \RuntimeException('Failed to export media to disk');
+            throw new RuntimeException('Failed to export media to disk');
         }
 
         /** @phpstan-ignore-next-line - FFMpeg fluent API */
         $formattedMedia = $toDiskMedia->inFormat($format);
         if ($formattedMedia === null || ! is_object($formattedMedia)) {
-            throw new \RuntimeException('Failed to format media');
+            throw new RuntimeException('Failed to format media');
         }
 
         if (! method_exists($formattedMedia, 'save')) {
-            throw new \RuntimeException('Formatted media does not have save method');
+            throw new RuntimeException('Formatted media does not have save method');
         }
 
         $formattedMedia->save($file_new);
