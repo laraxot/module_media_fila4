7ee58fc7791be83726e0d5f9ae2f547ff3c10d34
marco76tv <marco.sottana@gmail.com>
2025-12-09 16:44:46 +0100

    .

--- database/factories/MediaFactory.php
+++ database/factories/MediaFactory.php
@@ -9,56 +9,16 @@ use Modules\Media\Models\Media;
 
 /**
  * Media Factory
-<<<<<<< HEAD
  *
  * Factory for creating Media model instances for testing and seeding.
  *
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
- *
- * Factory for creating Media model instances for testing and seeding.
- *
-=======
- * 
- * Factory for creating Media model instances for testing and seeding.
- * 
->>>>>>> a12f125f4a (.)
-=======
- *
- * Factory for creating Media model instances for testing and seeding.
- *
->>>>>>> b93ef594b4 (.)
-=======
- * 
- * Factory for creating Media model instances for testing and seeding.
- * 
->>>>>>> origin/develop
->>>>>>> e9b0959 (.)
  * @extends Factory<Media>
  */
 class MediaFactory extends Factory
 {
     /**
      * The name of the factory's corresponding model.
-<<<<<<< HEAD
      *
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-     *
-=======
-     * 
->>>>>>> a12f125f4a (.)
-=======
-     *
->>>>>>> b93ef594b4 (.)
-=======
-     * 
->>>>>>> origin/develop
->>>>>>> e9b0959 (.)
      * @var class-string<Media>
      */
     protected $model = Media::class;
@@ -74,23 +34,7 @@ class MediaFactory extends Factory
         $fileName = $this->faker->word();
         /** @var string $extension */
         $extension = $this->faker->randomElement(['jpg', 'png', 'pdf', 'doc']);
-<<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
-        
->>>>>>> a12f125f4a (.)
-=======
-
->>>>>>> b93ef594b4 (.)
-=======
-        
->>>>>>> origin/develop
->>>>>>> e9b0959 (.)
         return [
             'model_type' => 'App\\Models\\User',
             'model_id' => $this->faker->numberBetween(1, 100),
@@ -147,7 +91,6 @@ class MediaFactory extends Factory
     {
         $extension = (string) $this->faker->randomElement(['jpg', 'png', 'gif']);
         $fileName = (string) $this->faker->word();
-<<<<<<< HEAD
 
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -156,25 +99,6 @@ class MediaFactory extends Factory
             'file_name' => $fileName.'.'.$extension,
 =======
         return $this->state(fn(array $_attributes): array => [
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-        return $this->state(fn(array $_attributes): array => [
-=======
-        
-        return $this->state(fn (array $attributes): array => [
->>>>>>> a12f125f4a (.)
-=======
-
-        return $this->state(fn(array $_attributes): array => [
->>>>>>> b93ef594b4 (.)
-=======
-        
-        return $this->state(fn (array $attributes): array => [
->>>>>>> origin/develop
->>>>>>> e9b0959 (.)
             'mime_type' => $this->getMimeTypeFromExtension($extension),
             'file_name' => $fileName . '.' . $extension,
 >>>>>>> 5200b63 (.)
@@ -205,7 +129,6 @@ class MediaFactory extends Factory
     {
         $extension = (string) $this->faker->randomElement(['pdf', 'doc', 'docx']);
         $fileName = (string) $this->faker->word();
-<<<<<<< HEAD
 
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -214,25 +137,6 @@ class MediaFactory extends Factory
             'file_name' => $fileName.'.'.$extension,
 =======
         return $this->state(fn(array $_attributes): array => [
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-        return $this->state(fn(array $_attributes): array => [
-=======
-        
-        return $this->state(fn (array $attributes): array => [
->>>>>>> a12f125f4a (.)
-=======
-
-        return $this->state(fn(array $_attributes): array => [
->>>>>>> b93ef594b4 (.)
-=======
-        
-        return $this->state(fn (array $attributes): array => [
->>>>>>> origin/develop
->>>>>>> e9b0959 (.)
             'mime_type' => $this->getMimeTypeFromExtension($extension),
             'file_name' => $fileName . '.' . $extension,
 >>>>>>> 5200b63 (.)
@@ -272,20 +176,4 @@ class MediaFactory extends Factory
             default => 'application/octet-stream',
         };
     }
-<<<<<<< HEAD
 }
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-}
-=======
-}
->>>>>>> a12f125f4a (.)
-=======
-}
->>>>>>> b93ef594b4 (.)
-=======
-}
->>>>>>> origin/develop
->>>>>>> e9b0959 (.)
