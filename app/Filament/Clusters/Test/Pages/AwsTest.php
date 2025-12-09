36d01ffd23af152d8da6e1f6dc2a32df0154a7e6
marco76tv <marco.sottana@gmail.com>
2025-12-09 17:05:29 +0100

    .

--- app/Filament/Clusters/Test/Pages/AwsTest.php
+++ app/Filament/Clusters/Test/Pages/AwsTest.php
@@ -7,6 +7,7 @@ namespace Modules\Media\Filament\Clusters\Test\Pages;
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 use Aws\Exception\AwsException;
 use Aws\S3\S3Client;
 use Aws\Sts\StsClient;
@@ -30,6 +31,8 @@ use Filament\Schemas\Schema;
 >>>>>>> 446bc61 (.)
 =======
 >>>>>>> 36d5713 (.)
+=======
+>>>>>>> 1a0067b (.)
 use Filament\Schemas\Components\Tabs;
 use Filament\Schemas\Components\Tabs\Tab;
 use Filament\Schemas\Components\Actions;
@@ -38,18 +41,35 @@ use Filament\Schemas\Components\Section;
 use Override;
 use Filament\Actions\Action;
 use Filament\Forms\Components\Textarea;
+=======
+use Override;
+use Filament\Schemas\Schema;
+use Filament\Schemas\Components\Tabs;
+use Filament\Schemas\Components\Tabs\Tab;
+use Filament\Schemas\Components\Section;
+use Filament\Schemas\Components\Actions;
+use Filament\Actions\Action;
+use Filament\Forms\Components\ViewField;
+>>>>>>> 739e4b7 (.)
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\KeyValue;
 use Exception;
 use Aws\Exception\AwsException;
 use Aws\S3\S3Client;
 use Aws\Sts\StsClient;
+<<<<<<< HEAD
 use Filament\Schemas\Components;
 use Filament\Notifications\Notification;
 use Modules\Media\Filament\Clusters\Test;
 use Modules\Xot\Filament\Pages\XotBasePage;
 >>>>>>> 5200b63 (.)
 use function Safe\json_encode;
+=======
+use Filament\Forms\Components;
+use Filament\Notifications\Notification;
+use Modules\Media\Filament\Clusters\Test;
+use Modules\Xot\Filament\Pages\XotBasePage;
+>>>>>>> 739e4b7 (.)
 
 class AwsTest extends XotBasePage
 {
@@ -81,8 +101,11 @@ class AwsTest extends XotBasePage
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 <<<<<<< HEAD
+=======
+>>>>>>> 1a0067b (.)
 =======
     #[Override]
     public function form(Schema $schema): Schema
@@ -96,10 +119,14 @@ class AwsTest extends XotBasePage
             ]),
         ]);
     }
+<<<<<<< HEAD
 >>>>>>> 1999eb3 (.)
 >>>>>>> 446bc61 (.)
 =======
 >>>>>>> 36d5713 (.)
+=======
+>>>>>>> 739e4b7 (.)
+>>>>>>> 1a0067b (.)
 
 >>>>>>> 5200b63 (.)
     protected function getS3TestSchema(): array
@@ -121,6 +148,7 @@ class AwsTest extends XotBasePage
                             ->color('success')
                             ->action('testS3FileOperations'),
                     ])->fullWidth(),
+<<<<<<< HEAD
                     Textarea::make('s3_results')
                         ->label('S3 Test Results')
                         ->rows(10)
@@ -131,16 +159,23 @@ class AwsTest extends XotBasePage
                         ->default(fn() => json_encode($this->testResults['s3'] ?? [], JSON_PRETTY_PRINT)),
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 >>>>>>> 5200b63 (.)
 =======
+=======
+>>>>>>> 1a0067b (.)
 =======
                     ViewField::make('s3_results')
                         ->view('ui::filament.components.test-results')
                         ->viewData(fn() => ['results' => $this->testResults['s3'] ?? null]),
+<<<<<<< HEAD
 >>>>>>> 1999eb3 (.)
 >>>>>>> 446bc61 (.)
 =======
 >>>>>>> 36d5713 (.)
+=======
+>>>>>>> 739e4b7 (.)
+>>>>>>> 1a0067b (.)
                 ]),
         ];
     }
@@ -154,6 +189,7 @@ class AwsTest extends XotBasePage
                     Action::make('test_cloudfront_config')->action('testCloudFrontConfig'),
                     Action::make('test_signed_urls')->action('testCloudFrontSignedUrls'),
                 ]),
+<<<<<<< HEAD
                 Textarea::make('cloudfront_results')
                     ->label('CloudFront Test Results')
                     ->rows(10)
@@ -162,7 +198,15 @@ class AwsTest extends XotBasePage
                     ->default(fn () => json_encode($this->testResults['cloudfront'] ?? [], JSON_PRETTY_PRINT)),
 =======
                     ->default(fn() => json_encode($this->testResults['cloudfront'] ?? [], JSON_PRETTY_PRINT)),
+<<<<<<< HEAD
 >>>>>>> 5200b63 (.)
+=======
+=======
+                ViewField::make('cloudfront_results')
+                    ->view('ui::filament.components.test-results')
+                    ->viewData(fn() => ['results' => $this->testResults['cloudfront'] ?? null]),
+>>>>>>> 739e4b7 (.)
+>>>>>>> 1a0067b (.)
             ]),
         ];
     }
@@ -176,6 +220,7 @@ class AwsTest extends XotBasePage
                     Action::make('test_iam_credentials')->action('testIamCredentials'),
                     Action::make('test_iam_policies')->color('warning')->action('testIamPolicies'),
                 ]),
+<<<<<<< HEAD
                 Textarea::make('iam_results')
                     ->label('IAM Test Results')
                     ->rows(10)
@@ -184,7 +229,15 @@ class AwsTest extends XotBasePage
                     ->default(fn () => json_encode($this->testResults['iam'] ?? [], JSON_PRETTY_PRINT)),
 =======
                     ->default(fn() => json_encode($this->testResults['iam'] ?? [], JSON_PRETTY_PRINT)),
+<<<<<<< HEAD
 >>>>>>> 5200b63 (.)
+=======
+=======
+                ViewField::make('iam_results')
+                    ->view('ui::filament.components.test-results')
+                    ->viewData(fn() => ['results' => $this->testResults['iam'] ?? null]),
+>>>>>>> 739e4b7 (.)
+>>>>>>> 1a0067b (.)
             ]),
         ];
     }
@@ -199,6 +252,7 @@ class AwsTest extends XotBasePage
                         ->icon('heroicon-o-bolt')
                         ->action('runFullDiagnostic'),
                 ]),
+<<<<<<< HEAD
                 Textarea::make('full_results')
                     ->label('Full Diagnostic Results')
                     ->rows(15)
@@ -207,7 +261,15 @@ class AwsTest extends XotBasePage
                     ->default(fn () => json_encode($this->testResults['full'] ?? [], JSON_PRETTY_PRINT)),
 =======
                     ->default(fn() => json_encode($this->testResults['full'] ?? [], JSON_PRETTY_PRINT)),
+<<<<<<< HEAD
 >>>>>>> 5200b63 (.)
+=======
+=======
+                ViewField::make('full_results')
+                    ->view('ui::filament.components.test-results')
+                    ->viewData(fn() => ['results' => $this->testResults['full'] ?? null]),
+>>>>>>> 739e4b7 (.)
+>>>>>>> 1a0067b (.)
                 KeyValue::make('aws_config')->columnSpanFull()->state($this->getAwsConfig(...)),
             ]),
         ];
