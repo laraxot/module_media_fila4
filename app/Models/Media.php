1ec4a0d1d6259be45650e72756799b3e9c71dc19
marco76tv <marco.sottana@gmail.com>
2025-12-09 17:03:43 +0100

    .

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -252,7 +252,6 @@ use Spatie\MediaLibrary\MediaCollections\Models\Media as SpatieMedia;
  */
 class Media extends SpatieMedia
 {
-    /** @use HasFactory<ModulesMediaDatabaseFactoriesMediaFactory> */
     use \Modules\Xot\Models\Traits\HasXotFactory;
     use Updater;
 
