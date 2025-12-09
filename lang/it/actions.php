f0c414b200fa0072b57ee2df6acae1f07c1067f4
marco76tv <marco.sottana@gmail.com>
2025-12-09 17:13:56 +0100

    .

--- lang/it/actions.php
+++ lang/it/actions.php
@@ -6,15 +6,6 @@ return [
     'add_attachment' => [
         'title' => 'Allegati',
         'label' => 'Carica allegato',
-<<<<<<< HEAD
-<<<<<<< HEAD
         'fields' => [],
-=======
-        'fields' => [
-        ],
->>>>>>> 0a466ed (.)
-=======
-        'fields' => [],
->>>>>>> 37a2da6 (.)
     ],
 ];
