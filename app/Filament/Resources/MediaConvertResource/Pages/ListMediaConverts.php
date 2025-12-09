b25adf1ad107b2290f1fc2653cee3659bdabcf3f
marco76tv <marco.sottana@gmail.com>
2025-12-09 17:13:42 +0100

    .

--- app/Filament/Resources/MediaConvertResource/Pages/ListMediaConverts.php
+++ app/Filament/Resources/MediaConvertResource/Pages/ListMediaConverts.php
@@ -30,9 +30,6 @@ class ListMediaConverts extends XotBaseListRecords
      * @return array<string, Tables\Columns\Column>
      */
     #[Override]
-    /**
-     * @return array<string, mixed>
-     */
     public function getTableColumns(): array
     {
         return [
@@ -58,9 +55,6 @@ class ListMediaConverts extends XotBaseListRecords
      * @return array<string, BaseFilter>
      */
     #[Override]
-    /**
-     * @return array<string, mixed>
-     */
     public function getTableFilters(): array
     {
         return [
@@ -83,9 +77,6 @@ class ListMediaConverts extends XotBaseListRecords
      * @return array<string, Action|ActionGroup>
      */
     #[Override]
-    /**
-     * @return array<string, mixed>
-     */
     public function getTableActions(): array
     {
         return [
@@ -106,9 +97,6 @@ class ListMediaConverts extends XotBaseListRecords
      * @return array<string, BulkAction>
      */
     #[Override]
-    /**
-     * @return array<string, mixed>
-     */
     public function getTableBulkActions(): array
     {
         return [
