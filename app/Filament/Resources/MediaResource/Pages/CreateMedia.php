ccf18f7ec17e200d5ca28a5009c60297efe5965b
marco76tv <marco.sottana@gmail.com>
2025-12-09 16:57:33 +0100

    .

--- app/Filament/Resources/MediaResource/Pages/CreateMedia.php
+++ app/Filament/Resources/MediaResource/Pages/CreateMedia.php
@@ -7,7 +7,15 @@ namespace Modules\Media\Filament\Resources\MediaResource\Pages;
 use Modules\Media\Filament\Resources\MediaResource;
 use Modules\Xot\Filament\Resources\Pages\XotBaseCreateRecord;
 
+<<<<<<< HEAD
 class CreateMedia extends XotBaseCreateRecord
+=======
+<<<<<<< HEAD
+class CreateMedia extends XotBaseCreateRecord
+=======
+class CreateMedia extends \Modules\Xot\Filament\Resources\Pages\XotBaseCreateRecord
+>>>>>>> origin/develop
+>>>>>>> e9b0959 (.)
 {
     protected static string $resource = MediaResource::class;
 }
