6308a0a409a0be85c4af9bc6af84f5bf4b3df1fd
marco76tv <marco.sottana@gmail.com>
2025-12-10 00:23:14 +0100

    .

--- app/Http/Controllers/ConvertController.php
+++ app/Http/Controllers/ConvertController.php
@@ -12,7 +12,23 @@ class ConvertController extends Controller
     /**
      * Show the profile for the given user.
      */
+<<<<<<< HEAD
     public function __invoke(string|int $_id): View
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+    public function __invoke(string|int $_id): View
+=======
+    public function __invoke(string|int $id): View
+>>>>>>> a12f125f4a (.)
+=======
+    public function __invoke(string|int $_id): View
+>>>>>>> b93ef594b4 (.)
+=======
+    public function __invoke(string|int $id): View
+>>>>>>> origin/develop
+>>>>>>> e9b0959 (.)
     {
         /**
          * @phpstan-var view-string
--- database/seeders/MediaDatabaseSeeder.php
+++ database/seeders/MediaDatabaseSeeder.php
@@ -4,6 +4,20 @@ declare(strict_types=1);
 
 namespace Modules\Media\database\seeders;
 
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+
+>>>>>>> origin/develop
+>>>>>>> e9b0959 (.)
 use Illuminate\Database\Seeder;
 
 class MediaDatabaseSeeder extends Seeder
