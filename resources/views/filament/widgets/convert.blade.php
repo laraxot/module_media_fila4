3350b9759a2f2065abc19d625a78a581904cfb1d
marco76tv <marco.sottana@gmail.com>
2025-12-10 00:33:07 +0100

    .

--- resources/views/filament/widgets/convert.blade.php
+++ resources/views/filament/widgets/convert.blade.php
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 37a2da6 (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
->>>>>>> 0a466ed (.)
-=======
->>>>>>> 37a2da6 (.)
 <x-filament-widgets::widget>
     <x-filament::section>
         {{-- Widget content --}}
