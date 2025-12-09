fb1e5db7d09697dd3448fcb4cea244d2a25cdbf8
marco76tv <marco.sottana@gmail.com>
2025-12-09 16:15:12 +0100

    .

--- database/seeders/MediaDatabaseSeeder.php
+++ database/seeders/MediaDatabaseSeeder.php
@@ -4,13 +4,6 @@ declare(strict_types=1);
 
 namespace Modules\Media\database\seeders;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> 0a466ed (.)
-=======
->>>>>>> 37a2da6 (.)
 use Illuminate\Database\Seeder;
 
 class MediaDatabaseSeeder extends Seeder
