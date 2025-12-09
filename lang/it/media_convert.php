ee2c40dd183582863bf7670ef72952fa6b2aefff
marco76tv <marco.sottana@gmail.com>
2025-12-09 16:57:51 +0100

    .

--- lang/it/media_convert.php
+++ lang/it/media_convert.php
@@ -1,5 +1,6 @@
 <?php
 
+<<<<<<< HEAD
 declare(strict_types=1);
 
 return [
@@ -18,3 +19,25 @@ return [
         ],
     ],
 ];
+=======
+return array (
+  'navigation' => 
+  array (
+    'label' => 'media convert',
+    'group' => 'media convert',
+    'icon' => 'media convert',
+    'sort' => 20,
+  ),
+  'fields' => 
+  array (
+    'applyFilters' => 
+    array (
+      'label' => 'applyFilters',
+    ),
+    'toggleColumns' => 
+    array (
+      'label' => 'toggleColumns',
+    ),
+  ),
+);
+>>>>>>> 0a466ed (.)
