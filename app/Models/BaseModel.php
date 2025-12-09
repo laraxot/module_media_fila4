19ab78764c9dac12c8e0b17124257a450b5f5939
marco76tv <marco.sottana@gmail.com>
2025-12-09 17:05:27 +0100

    .

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -4,21 +4,84 @@ declare(strict_types=1);
 
 namespace Modules\Media\Models;
 
-use Modules\Xot\Models\XotBaseModel;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Modules\Xot\Actions\Factory\GetFactoryAction;
+use Modules\Xot\Traits\Updater;
 
 /**
- * Base Model for Media module.
- *
- * Extends XotBaseModel which provides all standard properties and methods.
- *
- * @see \Modules\Xot\Models\XotBaseModel
+ * Class BaseModel.
  */
-abstract class BaseModel extends XotBaseModel
+abstract class BaseModel extends Model
 {
+    use HasFactory;
+
+    // use Searchable;
+    // //use Cachable;
+    use Updater;
+
     /**
-     * The connection name for the model.
+     * Indicates whether attributes are snake cased on arrays.
      *
-     * @var string
+     * @see https://laravel-news.com/6-eloquent-secrets
+     *
+     * @var bool
      */
+    public static $snakeAttributes = true;
+
+    /** @var bool */
+    public $incrementing = true;
+
+    /** @var bool */
+    public $timestamps = true;
+
+    /** @var int */
+    protected $perPage = 30;
+
+    /** @var string */
     protected $connection = 'media';
+
+    /** @var list<string> */
+    protected $fillable = [
+        'id',
+    ];
+
+    /** @var string */
+    protected $primaryKey = 'id';
+
+    /** @var string */
+    protected $keyType = 'string';
+
+    /** @var list<string> */
+    protected $hidden = [
+        // 'password'
+    ];
+
+    /**
+     * ----
+     * Create a new factory instance for the model.
+     *
+     * @return Factory<static>
+     */
+    protected static function newFactory()
+    {
+        return app(GetFactoryAction::class)->execute(static::class);
+    }
+
+    /** @return array<string, string> */
+    protected function casts(): array
+    {
+        return [
+            'id' => 'string',
+            'uuid' => 'string',
+            'published_at' => 'datetime',
+            'created_at' => 'datetime',
+            'updated_at' => 'datetime',
+            'deleted_at' => 'datetime',
+            'updated_by' => 'string',
+            'created_by' => 'string',
+            'deleted_by' => 'string',
+        ];
+    }
 }
