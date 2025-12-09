d0e49b6456a90881868ac2c57e0bcd88d5041e72
marco76tv <marco.sottana@gmail.com>
2025-12-09 17:05:20 +0100

    .

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -9,7 +9,6 @@ declare(strict_types=1);
 namespace Modules\Media\Models;
 
 use Eloquent;
-use Exception;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection as EloquentCollection;
 use Illuminate\Database\Eloquent\Factories\Factory;
@@ -20,7 +19,6 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Carbon;
 use Modules\Media\Database\Factories\MediaFactory;
 use Modules\Media\Enums\AttachmentTypeEnum;
-use Modules\Xot\Actions\Factory\GetFactoryAction;
 use Modules\Xot\Contracts\ProfileContract;
 use Modules\Xot\Contracts\UserContract;
 use Modules\Xot\Datas\XotData;
@@ -31,82 +29,82 @@ use Spatie\MediaLibrary\MediaCollections\Models\Media as SpatieMedia;
 /**
  * Modules\Media\Models\Media.
  *
- * @property int $id
- * @property string $model_type
- * @property string $model_id
- * @property string|null $uuid
- * @property string $collection_name
- * @property string $name
- * @property string $file_name
- * @property string|null $mime_type
- * @property string $disk
- * @property string|null $conversions_disk
- * @property int $size
- * @property array|null $manipulations
- * @property array|null $custom_properties
- * @property array|null $generated_conversions
- * @property array|null $responsive_images
- * @property int|null $order_column
- * @property Carbon|null $created_at
- * @property Carbon|null $updated_at
- * @property string|null $created_by
- * @property string|null $updated_by
- * @property int|null $user_id
- * @property string $directory
- * @property string|null $path
- * @property int|null $width
- * @property int|null $height
- * @property string|null $type
- * @property string|null $ext
- * @property string|null $alt
- * @property string|null $title
- * @property string|null $description
- * @property string|null $caption
- * @property string|null $exif
- * @property string|null $curations
- * @property UserContract|null $creator
- * @property Model|Eloquent $model
+ * @property int                  $id
+ * @property string               $model_type
+ * @property string               $model_id
+ * @property string|null          $uuid
+ * @property string               $collection_name
+ * @property string               $name
+ * @property string               $file_name
+ * @property string|null          $mime_type
+ * @property string               $disk
+ * @property string|null          $conversions_disk
+ * @property int                  $size
+ * @property array|null           $manipulations
+ * @property array|null           $custom_properties
+ * @property array|null           $generated_conversions
+ * @property array|null           $responsive_images
+ * @property int|null             $order_column
+ * @property Carbon|null          $created_at
+ * @property Carbon|null          $updated_at
+ * @property string|null          $created_by
+ * @property string|null          $updated_by
+ * @property int|null             $user_id
+ * @property string               $directory
+ * @property string|null          $path
+ * @property int|null             $width
+ * @property int|null             $height
+ * @property string|null          $type
+ * @property string|null          $ext
+ * @property string|null          $alt
+ * @property string|null          $title
+ * @property string|null          $description
+ * @property string|null          $caption
+ * @property string|null          $exif
+ * @property string|null          $curations
+ * @property UserContract|null    $creator
+ * @property Model|\Eloquent      $model
  * @property TemporaryUpload|null $temporaryUpload
  *
  * @method static MediaCollection<int, static> all($columns = ['*'])
  * @method static MediaCollection<int, static> get($columns = ['*'])
- * @method static Builder|Media newModelQuery()
- * @method static Builder|Media newQuery()
- * @method static Builder|Media ordered()
- * @method static Builder|Media query()
- * @method static Builder|Media whereAlt($value)
- * @method static Builder|Media whereCaption($value)
- * @method static Builder|Media whereCollectionName($value)
- * @method static Builder|Media whereConversionsDisk($value)
- * @method static Builder|Media whereCreatedAt($value)
- * @method static Builder|Media whereCreatedBy($value)
- * @method static Builder|Media whereCurations($value)
- * @method static Builder|Media whereCustomProperties($value)
- * @method static Builder|Media whereDescription($value)
- * @method static Builder|Media whereDirectory($value)
- * @method static Builder|Media whereDisk($value)
- * @method static Builder|Media whereExif($value)
- * @method static Builder|Media whereExt($value)
- * @method static Builder|Media whereFileName($value)
- * @method static Builder|Media whereGeneratedConversions($value)
- * @method static Builder|Media whereHeight($value)
- * @method static Builder|Media whereId($value)
- * @method static Builder|Media whereManipulations($value)
- * @method static Builder|Media whereMimeType($value)
- * @method static Builder|Media whereModelId($value)
- * @method static Builder|Media whereModelType($value)
- * @method static Builder|Media whereName($value)
- * @method static Builder|Media whereOrderColumn($value)
- * @method static Builder|Media wherePath($value)
- * @method static Builder|Media whereResponsiveImages($value)
- * @method static Builder|Media whereSize($value)
- * @method static Builder|Media whereTitle($value)
- * @method static Builder|Media whereType($value)
- * @method static Builder|Media whereUpdatedAt($value)
- * @method static Builder|Media whereUpdatedBy($value)
- * @method static Builder|Media whereUserId($value)
- * @method static Builder|Media whereUuid($value)
- * @method static Builder|Media whereWidth($value)
+ * @method static Builder|Media                newModelQuery()
+ * @method static Builder|Media                newQuery()
+ * @method static Builder|Media                ordered()
+ * @method static Builder|Media                query()
+ * @method static Builder|Media                whereAlt($value)
+ * @method static Builder|Media                whereCaption($value)
+ * @method static Builder|Media                whereCollectionName($value)
+ * @method static Builder|Media                whereConversionsDisk($value)
+ * @method static Builder|Media                whereCreatedAt($value)
+ * @method static Builder|Media                whereCreatedBy($value)
+ * @method static Builder|Media                whereCurations($value)
+ * @method static Builder|Media                whereCustomProperties($value)
+ * @method static Builder|Media                whereDescription($value)
+ * @method static Builder|Media                whereDirectory($value)
+ * @method static Builder|Media                whereDisk($value)
+ * @method static Builder|Media                whereExif($value)
+ * @method static Builder|Media                whereExt($value)
+ * @method static Builder|Media                whereFileName($value)
+ * @method static Builder|Media                whereGeneratedConversions($value)
+ * @method static Builder|Media                whereHeight($value)
+ * @method static Builder|Media                whereId($value)
+ * @method static Builder|Media                whereManipulations($value)
+ * @method static Builder|Media                whereMimeType($value)
+ * @method static Builder|Media                whereModelId($value)
+ * @method static Builder|Media                whereModelType($value)
+ * @method static Builder|Media                whereName($value)
+ * @method static Builder|Media                whereOrderColumn($value)
+ * @method static Builder|Media                wherePath($value)
+ * @method static Builder|Media                whereResponsiveImages($value)
+ * @method static Builder|Media                whereSize($value)
+ * @method static Builder|Media                whereTitle($value)
+ * @method static Builder|Media                whereType($value)
+ * @method static Builder|Media                whereUpdatedAt($value)
+ * @method static Builder|Media                whereUpdatedBy($value)
+ * @method static Builder|Media                whereUserId($value)
+ * @method static Builder|Media                whereUuid($value)
+ * @method static Builder|Media                whereWidth($value)
  *
  * @property mixed $extension
  * @property mixed $human_readable_size
@@ -127,8 +125,8 @@ use Spatie\MediaLibrary\MediaCollections\Models\Media as SpatieMedia;
  *
  * @method static MediaCollection<int, static> all($columns = ['*'])
  * @method static MediaCollection<int, static> get($columns = ['*'])
- * @method static Builder|Media whereDeletedAt($value)
- * @method static Builder|Media whereDeletedBy($value)
+ * @method static Builder|Media                whereDeletedAt($value)
+ * @method static Builder|Media                whereDeletedBy($value)
  * @method static MediaCollection<int, static> all($columns = ['*'])
  * @method static MediaCollection<int, static> get($columns = ['*'])
  * @method static MediaCollection<int, static> all($columns = ['*'])
@@ -142,9 +140,9 @@ use Spatie\MediaLibrary\MediaCollections\Models\Media as SpatieMedia;
  * @method static MediaCollection<int, static> all($columns = ['*'])
  * @method static MediaCollection<int, static> get($columns = ['*'])
  *
- * @property array $entry_conversions
+ * @property array                                 $entry_conversions
  * @property EloquentCollection<int, MediaConvert> $mediaConverts
- * @property int|null $media_converts_count
+ * @property int|null                              $media_converts_count
  *
  * @method static MediaCollection<int, static> all($columns = ['*'])
  * @method static MediaCollection<int, static> get($columns = ['*'])
@@ -245,16 +243,14 @@ use Spatie\MediaLibrary\MediaCollections\Models\Media as SpatieMedia;
  * @method static MediaCollection<int, static> get($columns = ['*'])
  * @method static MediaCollection<int, static> all($columns = ['*'])
  * @method static MediaCollection<int, static> get($columns = ['*'])
- *
- * @mixin IdeHelperMedia
- *
- * @method static MediaFactory factory($count = null, $state = [])
+ * @method static MediaFactory                 factory($count = null, $state = [])
  *
  * @mixin Eloquent
  */
 class Media extends SpatieMedia
 {
-    use HasFactory;
+    /** @use HasFactory<ModulesMediaDatabaseFactoriesMediaFactory> */
+    use \Modules\Xot\Models\Traits\HasXotFactory;
     use Updater;
 
     /** @var string */
@@ -312,7 +308,7 @@ class Media extends SpatieMedia
         $url = $this->getUrl();
         $info = pathinfo($url);
         if (! isset($info['dirname'])) {
-            throw new Exception('['.__LINE__.']['.class_basename($this).']');
+            throw new \Exception('['.__LINE__.']['.class_basename($this).']');
         }
         $url = '#';
         switch ($conv) {
@@ -333,6 +329,9 @@ class Media extends SpatieMedia
         return url($url);
     }
 
+    /**
+     * @return array<string, mixed>
+     */
     public function getEntryConversionsAttribute(): array
     {
         $conversions = [];
@@ -366,15 +365,5 @@ class Media extends SpatieMedia
             'generated_conversions' => 'array',
             'responsive_images' => 'array',
         ];
-    }
-
-    /**
-     * Create a new factory instance for the model.
-     *
-     * @return Factory<static>
-     */
-    protected static function newFactory()
-    {
-        return app(GetFactoryAction::class)->execute(static::class);
     }
 }
