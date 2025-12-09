048c9e7dead5d31b9ea0978dc04b11a0b78dee89
marco76tv <marco.sottana@gmail.com>
2025-12-09 16:15:08 +0100

    .

--- app/Actions/CloudFront/GetCloudFrontSignedUrlAction.php
+++ app/Actions/CloudFront/GetCloudFrontSignedUrlAction.php
@@ -8,6 +8,10 @@ use Aws\CloudFront\CloudFrontClient;
 use Modules\Media\Datas\CloudFrontData;
 use Spatie\QueueableAction\QueueableAction;
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> 0a466ed (.)
 /**
  * Action per la traduzione di elementi di una collezione.
  */
@@ -15,6 +19,7 @@ class GetCloudFrontSignedUrlAction
 {
     use QueueableAction;
 
+<<<<<<< HEAD
     public function execute(string $key, int $expiry = 30): string
     {
         $data = CloudFrontData::make();
@@ -22,13 +27,35 @@ class GetCloudFrontSignedUrlAction
         $cloudFront = new CloudFrontClient([
             'region' => $data->region,
             'version' => 'latest',
+=======
+   
+    public function execute(string $key, int $expiry = 30): string
+    {
+        $data = CloudFrontData::make();
+        
+        
+        $cloudFront = new CloudFrontClient([
+            'region' => $data->region,
+            'version' => 'latest'
+>>>>>>> 0a466ed (.)
         ]);
 
         return $cloudFront->getSignedUrl([
             'url' => $data->base_url.'/'.ltrim($key, '/'),
             'expires' => time() + ($expiry * 60),
             'key_pair_id' => $data->key_pair_id,
+<<<<<<< HEAD
             'private_key' => $data->getPrivateKey(),
         ]);
     }
+=======
+            'private_key' => $data->getPrivateKey()
+            ,
+        ]);
+    }
+
+   
+
+  
+>>>>>>> 0a466ed (.)
 }
