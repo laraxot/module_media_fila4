05705e52735b192599fbb6a23f89bc7df4943def
marco76tv <marco.sottana@gmail.com>
2025-12-10 09:38:51 +0100

    .

--- lang/it/icon_media.php
+++ lang/it/icon_media.php
@@ -1,5 +1,6 @@
 <?php
 
+<<<<<<< HEAD
 declare(strict_types=1);
 
 return [
@@ -30,3 +31,38 @@ return [
         ],
     ],
 ];
+=======
+return array (
+  'fields' => 
+  array (
+    'change-state' => 
+    array (
+      'label' => 'Cambia stato',
+      'placeholder' => 'Seleziona il nuovo stato',
+      'help' => 'Modifica lo stato corrente dell\'elemento',
+      'description' => 'Azione per cambiare lo stato',
+      'helper_text' => '',
+    ),
+    'state' => 
+    array (
+      'label' => 'Stato',
+      'placeholder' => 'Seleziona uno stato',
+      'help' => 'Stato attuale dell\'elemento',
+      'description' => 'Stato corrente del sistema',
+      'helper_text' => '',
+    ),
+    'message' => 
+    array (
+      'label' => 'Messaggio',
+      'placeholder' => 'Inserisci un messaggio',
+      'help' => 'Messaggio informativo per l\'utente',
+      'description' => 'Testo del messaggio',
+      'helper_text' => '',
+    ),
+    'open_link' => 
+    array (
+      'label' => 'open_link',
+    ),
+  ),
+);
+>>>>>>> 0a466ed (.)
