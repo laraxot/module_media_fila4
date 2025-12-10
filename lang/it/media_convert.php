b025b78647c28b4f1bccf4c080064d5f1d900f11
marco76tv <marco.sottana@gmail.com>
2025-12-10 18:09:22 +0100

    .

--- lang/it/media_convert.php
+++ lang/it/media_convert.php
@@ -1,5 +1,13 @@
 <?php
 
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b93ef594b4 (.)
+>>>>>>> e9b0959 (.)
 declare(strict_types=1);
 
 return [
@@ -18,3 +26,36 @@ return [
         ],
     ],
 ];
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+=======
+>>>>>>> origin/develop
+return array (
+  'navigation' => 
+  array (
+    'label' => 'media convert',
+    'group' => 'media convert',
+    'icon' => 'media convert',
+    'sort' => 20,
+  ),
+  'fields' => 
+  array (
+    'applyFilters' => 
+    array (
+      'label' => 'applyFilters',
+    ),
+    'toggleColumns' => 
+    array (
+      'label' => 'toggleColumns',
+    ),
+  ),
+);
+<<<<<<< HEAD
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+>>>>>>> origin/develop
+>>>>>>> e9b0959 (.)
