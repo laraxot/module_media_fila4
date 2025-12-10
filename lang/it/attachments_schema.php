9f27969ed4510ef76b8e3561de6d55dadb2b5e11
marco76tv <marco.sottana@gmail.com>
2025-12-10 09:37:20 +0100

    .

--- lang/it/attachments_schema.php
+++ lang/it/attachments_schema.php
@@ -1,6 +1,9 @@
 <?php
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 37a2da6 (.)
 declare(strict_types=1);
 
 return [
@@ -107,6 +110,7 @@ return [
         'delete_error' => 'Errore durante l\'eliminazione del file',
     ],
 ];
+<<<<<<< HEAD
 =======
 return array (
   'fields' => 
@@ -233,3 +237,5 @@ return array (
   ),
 );
 >>>>>>> 0a466ed (.)
+=======
+>>>>>>> 37a2da6 (.)
