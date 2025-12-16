36b122f3286ab789c46e60cf08058db2beb8f359
marco76tv <marco.sottana@gmail.com>
2025-12-16 19:44:02 +0100

    .

--- resources/views/filament/clusters/test/pages/s3-test.blade.php
+++ resources/views/filament/clusters/test/pages/s3-test.blade.php
@@ -3,11 +3,7 @@
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
 <x-filament::page>
-=======
-<x-fi<x-filament::page>
->>>>>>> e0ce06e (.)
     @php
         // Ensure $results is defined with a default value
         $results = $results ?? null;
