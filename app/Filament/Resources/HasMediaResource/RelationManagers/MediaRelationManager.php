67e0c4c226c81f72979750c24c6736bcd6644ef5
marco76tv <marco.sottana@gmail.com>
2025-12-16 19:44:59 +0100

    .

--- app/Filament/Resources/HasMediaResource/RelationManagers/MediaRelationManager.php
+++ app/Filament/Resources/HasMediaResource/RelationManagers/MediaRelationManager.php
@@ -16,13 +16,7 @@ class MediaRelationManager extends XotBaseRelationManager
 
     protected static ?string $inverseRelationship = 'model';
 
-    /**
-     * @return array<string, Action|ActionGroup>
-     */
     #[Override]
-    /**
-     * @return array<string, mixed>
-     */
     public function getTableHeaderActions(): array
     {
         return [
