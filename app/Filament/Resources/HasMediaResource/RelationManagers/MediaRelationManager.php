afa3cbcecf326efd996abbc42b094cbf0ea62c6b
marco76tv <marco.sottana@gmail.com>
2025-12-16 19:40:37 +0100

    .

--- app/Filament/Resources/HasMediaResource/RelationManagers/MediaRelationManager.php
+++ app/Filament/Resources/HasMediaResource/RelationManagers/MediaRelationManager.php
@@ -145,9 +145,6 @@ class MediaRelationManager extends XotBaseRelationManager
      * @return array<string, Action|ActionGroup>
      */
     #[Override]
-    /**
-     * @return array<string, mixed>
-     */
     public function getTableHeaderActions(): array
     {
         return [
