408aed94fc4b37e1d0c615dd06d1805e951fb2e3
marco76tv <marco.sottana@gmail.com>
2025-12-23 13:18:58 +0100

    .

--- lang/it/media.php
+++ lang/it/media.php
@@ -1,9 +1,5 @@
 <?php
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 37a2da6 (.)
 declare(strict_types=1);
 
 return [
@@ -58,71 +54,3 @@ return [
         'label' => 'media.model',
     ],
 ];
-<<<<<<< HEAD
-=======
-return array (
-  'pages' => 'Pagine',
-  'widgets' => 'Widgets',
-  'navigation' => 
-  array (
-    'name' => 'Media',
-    'plural' => 'Media',
-    'group' => 
-    array (
-      'name' => 'Sistema',
-      'description' => 'Gestione dei file multimediali',
-    ),
-    'label' => 'media',
-    'sort' => 20,
-    'icon' => 'media-main-animated',
-  ),
-  'fields' => 
-  array (
-    'name' => 'Nome',
-    'guard_name' => 'Guard',
-    'collection_name' => 'Collezione',
-    'filename' => 'Nome File',
-    'mime_type' => 'Tipo',
-    'human_readable_size' => 'Dimensione',
-    'permissions' => 'Permessi',
-    'updated_at' => 'Aggiornato il',
-    'first_name' => 'Nome',
-    'last_name' => 'Cognome',
-    'select_all' => 
-    array (
-      'name' => 'Seleziona Tutti',
-      'message' => '',
-    ),
-    'creator' => 
-    array (
-      'name' => 'Creatore',
-    ),
-    'uploaded_at' => 'Caricato il',
-  ),
-  'actions' => 
-  array (
-    'import' => 
-    array (
-      'fields' => 
-      array (
-        'import_file' => 'Seleziona un file XLS o CSV da caricare',
-      ),
-    ),
-    'export' => 
-    array (
-      'filename_prefix' => 'Aree al',
-      'columns' => 
-      array (
-        'name' => 'Nome area',
-        'parent_name' => 'Nome area livello superiore',
-      ),
-    ),
-  ),
-  'model' => 
-  array (
-    'label' => 'media.model',
-  ),
-);
->>>>>>> 0a466ed (.)
-=======
->>>>>>> 37a2da6 (.)
